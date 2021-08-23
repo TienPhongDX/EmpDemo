06b91f77e19e7a5ecff404188400aac1ea823e69
Nguyen Tien Phong <42340547+TienPhongDX@users.noreply.github.com>
2021-08-23 16:00:07 +0700

    Update EmployeeController.java

--- src/main/java/com/empExample/demo/controller/EmployeeController.java
+++ src/main/java/com/empExample/demo/controller/EmployeeController.java
@@ -16,29 +16,6 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 
-	@GetMapping("/employee")
-	public String index(Model model) {
-		model.addAttribute("employees", employeeService.findAll());
-		return "list";
-	}
-
-	@GetMapping("/employee/create")
-	public String create(Model model) {
-		model.addAttribute("employee", new Employee());
-		return "form";
-	}
-	@GetMapping("/employee/{id}/edit")
-	public String edit(@PathVariable int id, Model model) {
-		model.addAttribute("employee", employeeService.findOne(id));
-		return "form";
-	}
-	@PostMapping("/employee/search")
-	public String search(@RequestParam("s") String s, Model model) {
-		if (s.equals("")) {
-			return "redirect:/employee";
-		}
-		model.addAttribute("employees", employeeService.search(s));
-		return "list";
-	}
+	
 	
 }
