a72d8f756916e48859cf5205e0a2285b963166fb
Nguyen Tien Phong <42340547+TienPhongDX@users.noreply.github.com>
2021-08-25 09:55:42 +0700

    Update EmployeeController.java

--- src/main/java/com/empExample/demo/controller/EmployeeController.java
+++ src/main/java/com/empExample/demo/controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.empExample.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,15 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.empExample.demo.model.Employee;
 import com.empExample.demo.service.EmployeeService;
+import com.empExample.demo.service.EmployeeServiceImpl;
 
-@Controller
+
+@RestController
 public class EmployeeController {
+	
 	@Autowired
 	private EmployeeService employeeService;
 
+	@GetMapping("/emp")
+	public Iterable<Employee> EmployeeListAll(){ 
+		return employeeService.EmpList();
+	}
 	
 	
 }
