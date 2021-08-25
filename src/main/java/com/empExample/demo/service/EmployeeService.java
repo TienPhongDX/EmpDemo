cde8c0b953e81f93c7fd7dd02733bf5a26bf5159
Nguyen Tien Phong <42340547+TienPhongDX@users.noreply.github.com>
2021-08-25 10:17:32 +0700

    Update EmployeeService.java

--- src/main/java/com/empExample/demo/service/EmployeeService.java
+++ src/main/java/com/empExample/demo/service/EmployeeService.java
@@ -4,5 +4,19 @@ import com.empExample.demo.model.Employee;
 
 public interface EmployeeService {
 	Iterable<Employee> EmpList();
+	
+	Employee EmpAdd();
+	
+	Employee EmpItem(int id);
+	
+	Employee EmpUpdate(int id);
+	
+	Employee EmpChangePassword(int id);
+	
+	Employee EmpFilterById (int id);
+	
+	Iterable<Employee> EmpFilterByName (String name);
+	
+	String EmpDelete(int id);
 
 }
