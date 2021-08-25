057520c7536bd7304a4693664eea292049aea599
Nguyen Tien Phong <42340547+TienPhongDX@users.noreply.github.com>
2021-08-25 09:55:54 +0700

    Update EmployeeRepository.java

--- src/main/java/com/empExample/demo/repository/EmployeeRepository.java
+++ src/main/java/com/empExample/demo/repository/EmployeeRepository.java
@@ -3,12 +3,12 @@ package com.empExample.demo.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.empExample.demo.model.Employee;
 
+@Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
-	List<Employee> findByNameContaining(String q);
-	Employee findOne(int id);
 
 
 }
