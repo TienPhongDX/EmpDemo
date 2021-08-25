a0921d8ba4252f7ac382a2503246ca233b434995
Nguyen Tien Phong <42340547+TienPhongDX@users.noreply.github.com>
2021-08-25 09:56:04 +0700

    Update EmployeeService.java

--- src/main/java/com/empExample/demo/service/EmployeeService.java
+++ src/main/java/com/empExample/demo/service/EmployeeService.java
@@ -1,18 +1,8 @@
 package com.empExample.demo.service;
 
-import java.util.List;
-
 import com.empExample.demo.model.Employee;
 
 public interface EmployeeService {
-	Iterable<Employee> findAll();
-
-	List<Employee> search(String q);
-
-	Employee findOne(int id);
-
-	void save(Employee contact);
-
-	void delete(int id);
+	Iterable<Employee> EmpList();
 
 }
